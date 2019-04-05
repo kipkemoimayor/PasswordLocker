6e8d0588bea89e794df5515f1697e768e91687dc
kipkemoimayor <collinskipkemoi24@gmail.com>
2019-04-05 17:06:00 +0300

    This is to add a new empty list and declare a function setUp

--- user_login.py
+++ user_login.py
@@ -3,7 +3,8 @@ class User():
     """
         created an empty list to hold users info
     """
-    user_details=[];
-    def __init__(self, username,password):
+    user_details=[]; #empty
+    def __init__(self, username,password,email):
         self.username=username
         self.password=password
+        self.email=email
--- user_test.py
+++ user_test.py
@@ -1,2 +1,7 @@
 import user_login from User
 import unittest
+class UserTest(unittest.TestCase):
+    def setUp(self):
+        """
+            This is a set up function that runs every time before each test clauses
+        """
