d777730d6c96cf13c2d63bbb160dde222ad9a691
kipkemoimayor <collinskipkemoi24@gmail.com>
2019-04-05 15:49:31 +0300

    Created two files user_login and user_test.py

---
+++ user_login.py
@@ -0,0 +1,10 @@
+import random
+class User_login():
+    """
+        created an empty list to hold users info
+    """
+    user_details=[];
+    def __init__(self, username,password):
+        super(, self).__init__()
+        self.username=username
+        self.password=password
