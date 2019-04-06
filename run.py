246a26d6d7f1bb3e82d600e39c401a11528a4473
kipkemoimayor <collinskipkemoi24@gmail.com>
2019-04-06 12:25:23 +0300

    This is to add an authentication function

--- run.py
+++ run.py
@@ -1,6 +1,32 @@
 #!/usr/bin/env python3.6
 from user_login import User
-print("enter your user name")
-def getDetails(username):
-    print(username)
-getDetails(input())
+
+def register(username,password,accountName):
+    user_details=User(username,password,accountName)
+    return user_details
+def save_user(user_login):
+    user_login.register()
+
+def main():
+        print("**********\n***********\n********")
+        print("Welcome to password Locker Saves you password and hutsle of forgeting them")
+        print("Please Register\n Enter your userName")
+        name=input()
+        print("Enter your password")
+        password=input()
+        print("Enter your Your email")
+        email=input()
+        thisAccout="PasswordLPlus"
+        save_user(register(name,password,thisAccout))
+        print("\n")
+        print(f"Welcome {name}! Please Login ")
+        print("Enter your username that you just created")
+        vUser=input()
+        print("enter your password ")
+        vPassword=input()
+        if vUser ==password and vPassword==password:
+            print(f"Welcome to your dashboard {name}")
+        else:
+            print("wrong usermame or password")
+if __name__ == '__main__':
+    main()
